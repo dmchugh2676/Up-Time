0725d2c499571a66ba5bf93b39150181390f5727
dmchugh <damian@bulbs.ie>
2022-07-13 23:00:06 +0100

    first upload

---
+++ main.py
@@ -0,0 +1,38 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import mysql.connector
+
+# Connect to server
+cnx = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    user="root",
+    password="Wh@tsApp2017!")
+
+# Get a cursor
+cur = cnx.cursor()
+
+# Execute a query
+cur.execute("SELECT CURDATE()")
+
+# Fetch one result
+row = cur.fetchone()
+print("Current date is: {0}".format(row[0]))
+
+# Close connection
+cnx.close()
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
